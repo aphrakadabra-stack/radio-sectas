743a347505004cd2c334176142b916369e03346d
aphrakadabra-stack <aphrakadabra@gmail.com>
2026-08-22 00:31:12 -0300

    Use one notification context in the house

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ if (esNavegadorInstagram) {
 function abrirManifiesto() {
 
     if (!manifiestoCargado) {
-        marcoManifiesto.src = "manifiesto.html?inside=radio";
+        marcoManifiesto.src = "manifiesto.html?inside=radio&v=20260822-2";
         manifiestoCargado = true;
     }
 
